d816022f5264e20e853c3a88fae65a51a697610c
abdussalam <abdus.salam@clickchain.com>
2019-03-31 15:03:04 +0500

    Added transaction for saving challan information

--- src/main/java/com/eschool/domain/transaction/Transaction.java
+++ src/main/java/com/eschool/domain/transaction/Transaction.java
@@ -1,8 +1,18 @@
 package com.eschool.domain.transaction;
 
+import com.eschool.domain.date.OurDateTime;
+import com.eschool.domain.fee.FeeFrequency;
+
 /**
  * Created by Abdus Salam on 9/8/2018.
  */
 public abstract class Transaction {
 
+    private OurDateTime startDate;
+    private OurDateTime endDate;
+    private OurDateTime transactionDate;
+    private FeeFrequency feeFrequency;
+    private Double fee;
+    private Double admissionFee;
+    private Double printingFee;
 }
