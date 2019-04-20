aa9a376bd0050a464cb757f32eca949ecb56baa6
abdussalam <abdus.salam@clickchain.com>
2019-04-20 22:54:02 +0500

    created delegate to handle things

--- src/main/java/com/eschool/controller/FeeController.java
+++ src/main/java/com/eschool/controller/FeeController.java
@@ -1,5 +1,7 @@
 package com.eschool.controller;
 
+import com.eschool.delegate.FeeDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,11 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FeeController {
 
+    @Autowired
+    private FeeDelegate feeDelegate;
 
     @PostMapping
     public void generateFeeReportByFile(@PathVariable("file") MultipartFile file) {
-
+        feeDelegate.generateFeeReportByFile(file);
     }
 }
---
+++ src/main/java/com/eschool/delegate/FeeDelegate.java
@@ -0,0 +1,13 @@
+package com.eschool.delegate;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartFile;
+
+@Configuration
+public class FeeDelegate {
+
+
+    public void generateFeeReportByFile(MultipartFile file) {
+
+    }
+}
